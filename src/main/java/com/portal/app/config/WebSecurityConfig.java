5e0e1fb4b69df53a25039c0a53729d36503390b4
VejendlaRamya <95012644+VejendlaRamya@users.noreply.github.com>
2021-11-25 13:50:15 +0530

    Update WebSecurityConfig.java

--- src/main/java/com/portal/app/config/WebSecurityConfig.java
+++ src/main/java/com/portal/app/config/WebSecurityConfig.java
@@ -33,7 +33,7 @@ public JWTAuthorizationFilter authorizationTokenFilterBean() throws Exception {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().
                 authorizeRequests()
-                .antMatchers("/login" ,"/api/files/*").permitAll()
+                .antMatchers("/login").permitAll()
                 .anyRequest().authenticated()
                .and()
               .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
